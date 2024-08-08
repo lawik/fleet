79e8aa2dec08eca345e320f41b19163ae1b046c0
Lars Wikman <lars@underjord.io>
2024-08-08 21:04:05 +0200

    Lots of silliness

--- lib/fleet.ex
+++ lib/fleet.ex
@@ -3,16 +3,49 @@ defmodule Fleet do
   Documentation for Fleet.
   """
 
-  @doc """
-  Hello world.
+  def upload_data do
+    location = %{foo: "bla"}
 
-  ## Examples
+    data = Jason.encode!(location)
+    dbg(data)
+    presign = presign_post!()
+    dbg(presign)
+    serial = Nerves.Runtime.serial_number()
+    upload_data(presign, "geo.json", "data/#{serial}/location.json", data)
+  end
 
-      iex> Fleet.hello
-      :world
+  @open_secret "ti2zRfSCr3ITpMU9ReghbGvsy8EOW+VbfAfy18oe59o="
+  def presign_post!() do
+    serial = Nerves.Runtime.serial_number()
 
-  """
-  def hello do
-    :world
+    {:ok, %{body: %{"presigned_upload" => presign}}} =
+      Req.post("https://fleet-sign.fly.dev/sign",
+        json: %{"secret" => @open_secret, "serial_number" => serial}
+      )
+
+    presign
+  end
+
+  def upload_data(presigned_url, name, key, data) do
+    multipart =
+      Multipart.new()
+      |> Multipart.add_part(Multipart.Part.text_field(key, "key"))
+      |> Multipart.add_part(Multipart.Part.text_field("nerves-fleet-data", "bucket"))
+      |> Multipart.add_part(Multipart.Part.file_content_field(name, data, :file, filename: name))
+
+    content_length = Multipart.content_length(multipart)
+    content_type = Multipart.content_type(multipart, "multipart/form-data")
+
+    headers = [
+      {"Content-Type", content_type},
+      {"Content-Length", to_string(content_length)}
+    ]
+    dbg(headers)
+
+    Req.post(
+      presigned_url,
+      headers: headers,
+      body: Multipart.body_stream(multipart)
+    )
   end
 end
